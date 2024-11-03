e25a631121737b52a96c803d66c20726cdebd0e3
Andrey <legkij05@mail.ru>
2024-11-03 19:18:35 +0300

    added interface functionality to other classes

--- Service/src/main/java/org/example/TicketService.java
+++ Service/src/main/java/org/example/TicketService.java
@@ -3,24 +3,26 @@ package org.example;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class TicketService implements SetClassId {
+public class TicketService implements SetClassId, ClassInfo {
     private static int classID;
     public static void main(String[] args) {
 
     }
 
+    @Override
     public void print(){
 
-        System.out.println("Ticket cost: " + this.cost);
+        System.out.println("Class Id: " + classID);
     }
 
+    @Override
     public void print(FileWriter file) {
         if (file == null) {
             throw new IllegalArgumentException("FileWriter cannot be null");
         }
 
         try {
-            file.write("Class ID");
+            file.write("Class ID: " + classID);
         } catch (IOException e) {
             e.printStackTrace();
         }
