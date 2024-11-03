ed25f44a0e10202d2ddfdbb702b4308560e553d5
Andrey <legkij05@mail.ru>
2024-11-03 16:28:02 +0300

    Added class variables

--- Service/src/main/java/org/example/Ticket.java
+++ Service/src/main/java/org/example/Ticket.java
@@ -8,6 +8,23 @@ import java.util.Set;
 import java.math.BigDecimal;
 
 public class Ticket implements SetClassId{
+    private static int classID;
+    private static final int ID_LENGTH = 4;
+    private static final char[] CHAR_POOL = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray();
+    private static Set<String> generatedIDs = new HashSet<>();
+
+    private char[] ID;
+    private short eventCode;
+    private LocalDate creationDate;
+    private LocalTime creationTime;
+
+    private String concertHall;
+    private LocalDate day;
+    private LocalTime time;
+    private boolean isPromo;
+    private char stadiumSector;
+    private double maxWeight;
+    private BigDecimal cost;
 
 
 }
